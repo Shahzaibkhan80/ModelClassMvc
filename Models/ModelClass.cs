585c6f04f3e9a03f7dc0acdea8d5288ddc433795
Shahzaib Khan <13016@kiet.edu.pk>
2026-06-20 00:07:34 +0500

    craete a project mvc and create a modelClass file in model folder

---
+++ Models/ModelClass.cs
@@ -0,0 +1,10 @@
+﻿namespace ModelClass.Models
+{
+    public class ModelClass
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public double? Price { get; set; }
+        public int? Quantity { get; set; }
+    }
+}
